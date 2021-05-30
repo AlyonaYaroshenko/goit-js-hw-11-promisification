59c9a274c7ee118bf373208f7b4677fcc484b5f8
AlyonaYaroshenko <alyona.yaroshenko03@gmail.com>
2021-05-30 13:32:36 +0300

    finished task 1

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+// ## Задание 1
+
+// Напиши функцию `delay(ms)`, которая возвращает промис, 
+// переходящий в состояние`"resolved"` через `ms` миллисекунд. 
+// Значением исполнившегося промиса должно быть то
+// кол - во миллисекунд которое передали во время
+// вызова функции`delay`.
+
+// const delay = ms => {
+//   return new Promise((resolve, reject) => {
+//     const time = Math.random() > 0.5;
+//     setTimeout(() => {
+//       if (time) {
+//         resolve(`${ms}`);
+//       }
+//       reject('rejected');
+//     });
+//   });
+// };
+
 const delay = ms => {
-  // Твой код
+  return Promise.resolve(`${ms}`);
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
